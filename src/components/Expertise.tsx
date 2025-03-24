b9e1601f965c4628b5dda5d2077cb41bf844b22f
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-03-24 20:00:15 +0100

    Personalize pt1

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,56 +1,52 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faReact, faDocker, faPython, faJava, faJenkins} from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
 const labelsFirst = [
-    "React",
-    "TypeScript",
-    "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Flask",
+    "Spring Boot",
     "Python",
+    "Flask",
+    "JavaScript",
+    "React",
     "SQL",
     "PostgreSQL",
     "Postman"
 ];
 
 const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
+    "Python",
     "Pandas",
-    "Selenium",
+    "Torch",
+    "Tensorflow",
+    "Matplotlib",
+    "Sklearn",
+    "Hugging Face",
+    "Stable Baselines3"
 ];
 
 const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    "Docker",
+    "Github Actions",
+    "Jenkins",
+    "Gitlab pipelines",
+    "AWS",
+    "Git"
 ];
 
 function Expertise() {
     return (
     <div className="container" id="expertise">
         <div className="skills-container">
-            <h1>Expertise</h1>
+            <h1>Experience</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
+                    <FontAwesomeIcon icon={faJava} size="3x" className="fontawesomeicon"/>
+                    <FontAwesomeIcon icon={faPython} size="3x" className="fontawesomeicon"/>
+                    <h3>Web app development</h3>
+                    <p>I've worked on multiple web applications and have experience with microservice-centered development. I've helped with integrating Python's potential for ML applications with Spring Boot-based stack. While focusing on backend, I've also dipped my toes in some frontend technologies.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -60,9 +56,9 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <FontAwesomeIcon icon={faPython} size="3x" className="fontawesomeicon"/>
+                    <h3>Machine Learning</h3>
+                    <p>Ever since I've entered university, I've been passionately learning about modern AI. Both my engineering and master's thesis focused on machine learning. I've trained multiple supervised learning models, and have some experience with training agents based on reinforcement learning.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -72,9 +68,10 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <FontAwesomeIcon icon={faDocker} size="3x" className="fontawesomeicon"/>
+                    <FontAwesomeIcon icon={faJenkins} size="3x" className="fontawesomeicon"/>
+                    <h3>Automation</h3>
+                    <p>I have experience working with Dockerized applications, as well as setting up CI/CD using Github Actions and Jenkins to make sure all changes are up-to-date and working.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
