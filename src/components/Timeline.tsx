1f11dbeaf52ecff1acb2874d419eb7c20f394006
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-12-09 13:39:40 +0100

    Update timeline

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase, faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import '../assets/styles/Timeline.scss'
@@ -13,17 +13,31 @@ function Timeline() {
         <h1>Work experience</h1>
         <VerticalTimeline>
           <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+              contentArrowStyle={{ borderRight: '7px solid  white' }}
+              date="August - November 2025 (fixed-term contract)"
+              iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+              icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Junior Data Scientist</h3>
+            <h4 className="vertical-timeline-element-subtitle">Grodan</h4>
+            <p>
+              Amazon Sagemaker, Sagemaker Pipelines, Time Series Forecasting, Apache Iceberg, Autogluon, Pytorch, Pandas
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2024 - present"
+            date="2024 - 2025"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
             <h3 className="vertical-timeline-element-title">Master's Degree</h3>
             <h4 className="vertical-timeline-element-subtitle">Adam Mickiewicz University</h4>
             <p>
-              Python, NLP, Fine-tuning LLMs, MLOps, Reinforcement Learning, Sentiment Analysis
+              Python, NLP, Fine-tuning LLMs, MLOps, Reinforcement Learning, Behavioral Cloning, Sentiment Analysis, Time Series Forecasting
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -42,19 +56,19 @@ function Timeline() {
             className="vertical-timeline-element--work"
             date="2019 - 2023"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
             <h3 className="vertical-timeline-element-title">Engineering Degree</h3>
             <h4 className="vertical-timeline-element-subtitle">Adam Mickiewicz University</h4>
             <p>
-              Python, Java / Spring Boot, OpenGL, SQL, Recommender Systems, Javascript
+              Python, Java / Spring Boot, OpenGL, SQL, Recommender Systems, Javascript, Web scraping
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="2015 - 2019"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
+            icon={<FontAwesomeIcon icon={faGraduationCap} />}
           >
             <h3 className="vertical-timeline-element-title">Technical School</h3>
             <h4 className="vertical-timeline-element-subtitle">ZSK</h4>
